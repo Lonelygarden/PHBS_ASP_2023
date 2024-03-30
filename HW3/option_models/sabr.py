defbecab387c50021d8ac3c1ebb8483c86b5f185
Lonelygarden <2726187762@qq.com>
2024-03-30 20:49:27 +0800

    Final Delivery

--- HW3/option_models/sabr.py
+++ HW3/option_models/sabr.py
@@ -9,8 +9,9 @@ import numpy as np
 import pyfeng as pf
 import abc
 
+
 class ModelABC(abc.ABC):
-    beta = 1   # fixed (not used)
+    beta = 1  # fixed (not used)
     vov, rho = 0.0, 0.0
     sigma, intr = None, None
 
@@ -34,13 +35,13 @@ class ModelABC(abc.ABC):
         elif self.beta == 1:
             return pf.Bsm(sigma, intr=self.intr)
         else:
-            raise ValueError(f'0<beta<1 not supported')
+            raise ValueError(f"0<beta<1 not supported")
 
     def vol_smile(self, strike, spot, texp=1.0):
-        ''''
+        """'
         From the price from self.price() compute the implied vol
         Use self.bsm_model.impvol() method
-        '''
+        """
         price = self.price(strike, spot, texp, cp=1)
         iv = self.base_model().impvol(price, strike, spot, texp, cp=1)
         return iv
@@ -75,10 +76,12 @@ class ModelABC(abc.ABC):
         tobs = np.arange(1, n_dt + 1) / n_dt * texp
         dt = texp / n_dt
         assert texp == tobs[-1]
-        
+
         Z_1 = np.random.standard_normal((n_dt, self.n_path))
         Z_t = np.cumsum(Z_1 * np.sqrt(dt), axis=0)
-        sigma_t = np.exp(self.vov * (Z_t - self.vov/2 * tobs[:, None]))  # 此处是通过Z_t的cumsum来进行每步的累积的
+        sigma_t = np.exp(
+            self.vov * (Z_t - self.vov / 2 * tobs[:, None])
+        )  # 此处是通过Z_t的cumsum来进行每步的累积的
         sigma_t = np.insert(sigma_t, 0, np.ones(sigma_t.shape[1]), axis=0)
 
         return Z_1, sigma_t
@@ -97,20 +100,21 @@ class ModelABC(abc.ABC):
         weight = np.ones(sigma_path.shape[0])
         weight[[0, -1]] = 0.5
         weight /= weight.sum()
-        intvar = np.sum(weight[:, None] * sigma_path, axis=0)
+        intvar = np.sum(weight[:, None] * sigma_path * sigma_path, axis=0)
         return intvar
+
 
 class ModelBsmMC(ModelABC):
     """
     MC for Bsm SABR (beta = 1)
     """
 
-    beta = 1.0   # fixed (not used)
+    beta = 1.0  # fixed (not used)
 
     def price(self, strike, spot, texp=1.0, cp=1):
-        '''
+        """
         Your MC routine goes here.
-        (1) Generate the paths of sigma_t. 
+        (1) Generate the paths of sigma_t.
 
         vol_path = self.sigma_path(texp)  # the path of sigma_t
         sigma_t = vol_path[-1, :]  # sigma_t at maturity (t=T)
@@ -120,43 +124,55 @@ class ModelBsmMC(ModelABC):
         Z = np.random.standard_normal()
 
         (3) Calculate option prices (vector) for all strikes
-        '''
+        """
         # (1) Generate the paths of sigma_t.
-        Z_1, vol_path = self.sigma_path(texp)  # normal variable Z_1, the path of sigma_t
+        Z_1, vol_path = self.sigma_path(
+            texp
+        )  # normal variable Z_1, the path of sigma_t
         sigma_t = vol_path[-1, :]  # sigma_t at maturity (t=T)
-        
+
         # (2) Simulate S_0, ...., S_T.
         n_dt = int(np.ceil(texp / self.dt))
         tobs = np.arange(1, n_dt + 1) / n_dt * texp
         dt = texp / n_dt
         assert texp == tobs[-1]
 
-        X_1 = np.random.standard_normal((n_dt, self.n_path)) # normal variable X_1, indepent with Z_1
-        W_1 = self.rho*Z_1 + np.sqrt(1-self.rho**2)*X_1  # normal variable W_1, correlated with Z_1 of rho
-        #这里有一个问题，sigma_t和S_t会差一期，导致会有浪费的sigma_t
-        sigma_times_W_accumulation = np.cumsum(W_1 * np.sqrt(dt) * self.sigma * vol_path[:-1], axis=0)
-        sigma_squared_accumulation = np.cumsum(np.square(self.sigma * vol_path[:-1]), axis=0)
-        S_t = spot * np.exp(sigma_times_W_accumulation - 1/2*sigma_squared_accumulation*dt)
-        S_t = np.insert(S_t, 0, spot*np.ones(S_t.shape[1]), axis=0)
-        
-        
+        X_1 = np.random.standard_normal(
+            (n_dt, self.n_path)
+        )  # normal variable X_1, indepent with Z_1
+        W_1 = (
+            self.rho * Z_1 + np.sqrt(1 - self.rho**2) * X_1
+        )  # normal variable W_1, correlated with Z_1 of rho
+        # 这里有一个问题，sigma_t和S_t会差一期，导致会有浪费的sigma_t
+        sigma_times_W_accumulation = np.cumsum(
+            W_1 * np.sqrt(dt) * self.sigma * vol_path[:-1], axis=0
+        )
+        sigma_squared_accumulation = np.cumsum(
+            np.square(self.sigma * vol_path[:-1]), axis=0
+        )
+        S_t = spot * np.exp(
+            sigma_times_W_accumulation - 1 / 2 * sigma_squared_accumulation * dt
+        )
+        S_t = np.insert(S_t, 0, spot * np.ones(S_t.shape[1]), axis=0)
+
         # (3) Calculate option prices (vector) for all strikes
-        S_T = S_t[-1,:]
+        S_T = S_t[-1, :]
         df = np.exp(-self.intr * texp)
-        p = df * np.mean(np.fmax(cp*(S_T - strike[:, None]), 0.0), axis=1)
+        p = df * np.mean(np.fmax(cp * (S_T - strike[:, None]), 0.0), axis=1)
         return p
+
 
 class ModelNormMC(ModelBsmMC):
     """
     MC for Normal SABR (beta = 0)
     """
 
-    beta = 0   # fixed (not used)
+    beta = 0  # fixed (not used)
 
     def price(self, strike, spot, texp=1.0, cp=1):
-        '''
+        """
         Your MC routine goes here.
-        (1) Generate the paths of sigma_t. 
+        (1) Generate the paths of sigma_t.
 
         vol_path = self.sigma_path(texp)  # the path of sigma_t
         sigma_t = vol_path[-1, :]  # sigma_t at maturity (t=T)
@@ -166,9 +182,11 @@ class ModelNormMC(ModelBsmMC):
         Z = np.random.standard_normal()
 
         (3) Calculate option prices (vector) for all strikes
-        '''
+        """
         # (1) Generate the paths of sigma_t.
-        Z_1, vol_path = self.sigma_path(texp)  # normal variable Z_1, the path of sigma_t
+        Z_1, vol_path = self.sigma_path(
+            texp
+        )  # normal variable Z_1, the path of sigma_t
         sigma_t = vol_path[-1, :]  # sigma_t at maturity (t=T)
 
         # (2) Simulate S_0, ...., S_T.
@@ -177,19 +195,26 @@ class ModelNormMC(ModelBsmMC):
         dt = texp / n_dt
         assert texp == tobs[-1]
 
-        X_1 = np.random.standard_normal((n_dt, self.n_path)) # normal variable X_1, indepent with Z_1
-        W_1 = self.rho*Z_1 + np.sqrt(1-self.rho**2)*X_1  # normal variable W_1, correlated with Z_1 of rho
-        #这里有一个问题，sigma_t和S_t会差一期，导致会有浪费的sigma_t
-        #原路径初值为1，此处的vol_path需要乘sigma_0
-        sigma_times_W_accumulation = np.cumsum(W_1 * np.sqrt(dt) * self.sigma *  vol_path[:-1], axis=0) 
+        X_1 = np.random.standard_normal(
+            (n_dt, self.n_path)
+        )  # normal variable X_1, indepent with Z_1
+        W_1 = (
+            self.rho * Z_1 + np.sqrt(1 - self.rho**2) * X_1
+        )  # normal variable W_1, correlated with Z_1 of rho
+        # 这里有一个问题，sigma_t和S_t会差一期，导致会有浪费的sigma_t
+        # 原路径初值为1，此处的vol_path需要乘sigma_0
+        sigma_times_W_accumulation = np.cumsum(
+            W_1 * np.sqrt(dt) * self.sigma * vol_path[:-1], axis=0
+        )
         S_t = spot + sigma_times_W_accumulation
-        S_t = np.insert(S_t, 0, spot*np.ones(S_t.shape[1]), axis=0)
+        S_t = np.insert(S_t, 0, spot * np.ones(S_t.shape[1]), axis=0)
 
         # (3) Calculate option prices (vector) for all strikes
-        S_T = S_t[-1,:]
+        S_T = S_t[-1, :]
         df = np.exp(-self.intr * texp)
-        p = df * np.mean(np.fmax(cp*(S_T - strike[:, None]), 0.0), axis=1)
+        p = df * np.mean(np.fmax(cp * (S_T - strike[:, None]), 0.0), axis=1)
         return p
+
 
 class ModelBsmCondMC(ModelBsmMC):
     """
@@ -197,39 +222,41 @@ class ModelBsmCondMC(ModelBsmMC):
     """
 
     def price(self, strike, spot, texp=1.0, cp=1):
-        '''
+        """
         Your MC routine goes here.
         (1) Generate the paths of sigma_t and normalized integrated variance
 
         vol_path = self.sigma_path(texp)  # the path of sigma_t
         sigma_t = vol_path[-1, :]  # sigma_t at maturity (t=T)
-        I_t = self.intvar_normalized(vol_path) 
+        I_t = self.intvar_normalized(vol_path)
 
         (2) Calculate the equivalent spot and volatility of the BS model
 
-        vol = 
-        spot_equiv = 
+        vol =
+        spot_equiv =
 
         (3) Calculate option prices (vector) by averaging the BS prices
 
         m = self.base_model(vol)
         p = np.mean(m.price(strike[:, None], spot_equiv, texp, cp), axis=1)
-        '''
+        """
         # (1) Generate the paths of sigma_t and normalized integrated variance
         Z_1, vol_path = self.sigma_path(texp)  # the path of sigma_t
         sigma_t = self.sigma * vol_path[-1, :]  # sigma_t at maturity (t=T)
         I_t = self.intvar_normalized(vol_path)
 
         # (2) Calculate the equivalent spot and volatility of the BS model
-        
-        spot_equiv = spot * np.exp(self.rho/self.vov * (sigma_t-self.sigma) - 
-                                  self.rho**2 * self.sigma**2 * texp/2 * I_t)
-        vol = self.sigma * np.sqrt((1-self.rho**2) * I_t)
+
+        spot_equiv = spot * np.exp(
+            self.rho / self.vov * (sigma_t - self.sigma)
+            - self.rho**2 * self.sigma**2 * texp / 2 * I_t
+        )
+        vol = self.sigma * np.sqrt((1 - self.rho**2) * I_t)
 
         # (3) Calculate option prices (vector) by averaging the BS prices
-        m = self.base_model(vol)  #BSM model
+        m = self.base_model(vol)  # BSM model
         p = np.mean(m.price(strike[:, None], spot_equiv, texp, cp), axis=1)
-        
+
         return p
 
 
@@ -239,35 +266,35 @@ class ModelNormCondMC(ModelNormMC):
     """
 
     def price(self, strike, spot, texp=1.0, cp=1):
-        '''
+        """
         Your MC routine goes here.
         (1) Generate the paths of sigma_t and normalized integrated variance
 
         vol_path = self.sigma_path(texp)  # the path of sigma_t
         sigma_t = vol_path[-1, :]  # sigma_t at maturity (t=T)
-        I_t = self.intvar_normalized(vol_path) 
+        I_t = self.intvar_normalized(vol_path)
 
         (2) Calculate the equivalent spot and volatility of the Bachelier model
 
-        vol = 
-        spot_equiv = 
+        vol =
+        spot_equiv =
 
         (3) Calculate option prices (vector) by averaging the BS prices
 
         m = self.base_model(vol)
         p = np.mean(m.price(strike[:, None], spot_equiv, texp, cp), axis=1)
-        '''
+        """
         # (1) Generate the paths of sigma_t and normalized integrated variance
         Z_1, vol_path = self.sigma_path(texp)  # the path of sigma_t
         sigma_t = self.sigma * vol_path[-1, :]  # sigma_t at maturity (t=T)
         I_t = self.intvar_normalized(vol_path)
 
         # (2) Calculate the equivalent spot and volatility of the BS model
-        vol = self.sigma * np.sqrt((1-self.rho**2) * I_t)
-        spot_equiv = spot + self.rho/self.vov * (sigma_t-self.sigma)
+        vol = self.sigma * np.sqrt((1 - self.rho**2) * I_t)
+        spot_equiv = spot + self.rho / self.vov * (sigma_t - self.sigma)
 
         # (3) Calculate option prices (vector) by averaging the BS prices
         m = self.base_model(vol)
         p = np.mean(m.price(strike[:, None], spot_equiv, texp, cp), axis=1)
-        
+
         return p
